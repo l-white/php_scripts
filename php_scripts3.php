acdaf040f44ad3bb5c88d2395ddb7cdcf995ecc7
Laura <law7676@outlook.com>
2015-10-01 12:28:51 -0500

    tweaked loops

--- php_scripts3.php
+++ php_scripts3.php
@@ -2,6 +2,7 @@
 
 // an odd for loop for dot challenge
 
+// all dots indent one space
 for ($o = 1; $o <= 4; $o++) {
 for ($r = 1; $r <= $o; $r++) {
   echo "." . "\n" . " ";
@@ -10,10 +11,39 @@ for ($r = 1; $r <= $o; $r++) {
 
 echo "\n";
 
+// indent starts with second dot
+
 for ($h = 1; $h <= 4; $h++) {
   for ($h = 1; $h <= 4; $h++) {
     echo "." . "\n" . " ";
   }
 }
+
+echo "\n";
+
+$i = 1;
+$j = 1;
+while( $i < 3 )
+{
+    while( $j < 3 )
+    {
+        echo "-" . "\n";
+        $j++;
+    }
+    echo "outer\n";
+    $i++;
+}
+
+echo "\n";
+
+for( $i=1; $i<3; $i++)
+{
+   for( $j=1; $j<3; $j++)
+   {
+        echo "." . "\n";
+   }
+ }
+    echo "outer\n";
+
 
 ?>
