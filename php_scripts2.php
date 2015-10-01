0419ea7a5286fb036db3812df4457c767469e6a6
Laura <law7676@outlook.com>
2015-10-01 08:25:57 -0500

    second file for php scripts

---
+++ php_scripts2.php
@@ -0,0 +1,42 @@
+<?php
+
+$a = 4;
+
+if ($a > 5 || $a != 5){
+  echo "true";
+} else {
+  echo "false";
+}
+echo "\n";
+
+$b = 5;
+
+if ($b % 2 === 0) {
+	echo "$b is a multiple of 2";
+} elseif ($b % 3 === 0) {
+	echo "$b is a multiple of 3";
+} elseif ($b % 4 === 0) {
+	echo "$b is a multiple of 4";
+} else {
+	echo "$b is not a multiple of 2, 3, or 4.";
+}
+echo "\n";
+
+$userName = "Laura";
+
+if ($userName === "Laura") {
+  echo "Hello, me";
+} elseif ($userName === "Scot") {
+  echo "Hello, Scot";
+} else {
+  echo "Hello";
+}
+echo "\n";
+
+// for loop
+
+for ($i = 1; $i <= 10; $i++) {
+	echo $i . "\n";
+}
+
+?>
