d98faf383d7338701fb79f2134c5f46aa425a03a
Laura <law7676@outlook.com>
2015-10-01 11:51:28 -0500

    initial commit w/ odd for loop

---
+++ php_scripts3.php
@@ -0,0 +1,19 @@
+<?php
+
+// an odd for loop for dot challenge
+
+for ($o = 1; $o <= 4; $o++) {
+for ($r = 1; $r <= $o; $r++) {
+  echo "." . "\n" . " ";
+}
+}
+
+echo "\n";
+
+for ($h = 1; $h <= 4; $h++) {
+  for ($h = 1; $h <= 4; $h++) {
+    echo "." . "\n" . " ";
+  }
+}
+
+?>
