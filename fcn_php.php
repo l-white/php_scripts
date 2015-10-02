ac6b792b79c633fdd8d8e12de9457962cfcb7d91
Laura <law7676@outlook.com>
2015-10-02 11:31:50 -0500

    fcn example returning one value

--- fcn_php.php
+++ fcn_php.php
@@ -1,4 +1,36 @@
 <?php
+//Passing values through a function
+
+function multiply($num1, $num2) {
+  $result = $num1 * $num2;
+  echo $result;
+}
+
+multiply(5, 10);
+
+echo "\n";
+
+multiply(3, 7);
+
+echo "\n";
+
+
+//How to return a value from a function
+
+//IMPORTANT -- YOU CAN ONLY RETURN ONE VALUE FROM A FUNCTION LIKE THIS
+
+
+function uppercase($name1) {
+  $result = strtoupper($name);
+  return $result;
+}
+
+echo uppercase("White");
+
+// How to return multiple values from a function
+
+
+
 
 
 
