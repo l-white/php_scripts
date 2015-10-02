515f0284f8dc8de358fb323f1f429d9b43e18443
Laura <law7676@outlook.com>
2015-10-02 08:07:43 -0500

    added html form

---
+++ html_forms_php.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>HTML forms with PHP</title>
+  </head>
+
+  <body>
+
+    <form action="" method="post">
+      <input type="text" name="surname">
+      Male<input type="radio" name="gender" value="Male">
+      Female<input type="radio" name="gender" value="Female">
+      <input type="submit" name="submit" value="submit">
+    </form>
+
+  </body>
+
+</html>
